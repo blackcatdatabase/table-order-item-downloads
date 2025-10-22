c138b9b34028f2754562b7a9ec0d83d07f51e410
Tolben <vitek.pasek@gmail.com>
2025-10-22 20:58:42 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: order_item_downloads
 CREATE TABLE IF NOT EXISTS order_item_downloads (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: order_item_downloads
 ALTER TABLE order_item_downloads ADD CONSTRAINT fk_oid_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE;
 
