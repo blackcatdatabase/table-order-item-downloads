994f4b5b639d6ac23db4621de0fef2c954688916
Tolben <vitek.pasek@gmail.com>
2025-10-23 12:23:03 +0200

    chore(codegen): add/update generated PHP (Module, Joins, Repository, Mapper, Exceptions) from schema-map

--- src/Service/OrderItemDownloadsAggregateService.php
+++ src/Service/OrderItemDownloadsAggregateService.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace BlackCat\Database\Packages\OrderItemDownloads\Service;
 
-use BlackCat\Core\Database\Database;
+use BlackCat\Core\Database;
 use BlackCat\Database\Packages\OrderItemDownloads\Dto\OrderItemDownloadDto;
 use BlackCat\Database\Packages\OrderItemDownloads\Mapper\OrderItemDownloadDtoMapper;
 use BlackCat\Database\Packages\OrderItemDownloads\Repository\OrderItemDownloadRepository;
